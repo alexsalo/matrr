8cdb928db15859393bbeb5aa55e8bd099bcff659
soltau <soltau@8ba6b188-5121-491e-b013-2137287ea23c>
2011-05-05 20:16:37 +0000

    Changed media and static urls in settings.py to include the SITE_ROOT.
    Added SITE_ROOT to links in login.form and index.html.
    
    Changed the back link on static_page.html to use javascript:history.go(-1) instead of going back to the site homepage.
    
    
    git-svn-id: svn+ssh://gleek.ecs.baylor.edu/matrr/trunk@18 8ba6b188-5121-491e-b013-2137287ea23c

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ MEDIA_ROOT = '/web/django_test/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = SITE_ROOT + '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -95,7 +95,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = SITE_ROOT + '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
