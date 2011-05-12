5fc61a9adf108b8bce5dcaf9f335da1a19dc0565
soltau <soltau@8ba6b188-5121-491e-b013-2137287ea23c>
2011-05-12 21:23:45 +0000

    Changed the widget used for the unavailable_for part of the tissue classes in the admin interface.
    Updated TODO list.
    
    git-svn-id: svn+ssh://gleek.ecs.baylor.edu/matrr/trunk@54 8ba6b188-5121-491e-b013-2137287ea23c

--- matrr/admin.py
+++ matrr/admin.py
@@ -1,10 +1,15 @@
 from matrr.models import *
 from django.contrib import admin
 
-admin.site.register(TissueType)
+class TissueAdminForm(admin.ModelAdmin):
+  formfield_overrides = {
+    models.ManyToManyField: {'widget': FilteredSelectMultiple("Monkeys", is_stacked=False)},
+  }
+
+admin.site.register(TissueType, TissueAdminForm)
 admin.site.register(BrainBlock)
-admin.site.register(BrainRegion)
-admin.site.register(BloodAndGenetic)
+admin.site.register(BrainRegion, TissueAdminForm)
+admin.site.register(BloodAndGenetic, TissueAdminForm)
 admin.site.register(Cohort)
 admin.site.register(CohortEvent)
 admin.site.register(EventType)
