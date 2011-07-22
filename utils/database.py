1d63d115352cd35687f7d04a40372320d63b8310
soltau <soltau@8ba6b188-5121-491e-b013-2137287ea23c>
2011-07-22 19:04:11 +0000

    Updated the load_experiments function.
    git-svn-id: svn+ssh://gleek.ecs.baylor.edu/matrr/trunk@114 8ba6b188-5121-491e-b013-2137287ea23c

--- utils/database.py
+++ utils/database.py
@@ -9,7 +9,7 @@ def load_experiments(file):
   This function will assume that the monkeys and cohorts are already entered into the database.
   Assumes the columns are in the following order:
     0 - Date
-    1 - Cohort
+    1 - Cohort - this is ignored.  we look at the monkey's cohort instead
     2 - Experiment Type
     3 - Monkey ID
     4 - EtOH Intake
@@ -18,25 +18,31 @@ def load_experiments(file):
     7 - Total Pellets Consumed
     8 - Weight
   """
-  input = csv.reader(open( file, 'rU'), delimiter='\t', quoting=csv.QUOTE_NONE)
+  input = csv.reader(open( file, 'rU'), delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
   # get the column headers
   columns = input.next()
   
   for row in input:
+    print row
+    # get the monkey
+    monkey = Monkey.objects.get(mky_real_id=row[3])
     # create the drinking_experiment if it does not already exist
-    if DrinkingExperiment.objects.filter(dex_date=row[0], cohort=row[1], dex_type=row[2]).count() == 0:
-      drinking_experiment = DrinkingExperiment(dex_date=row[0], cohort=row[1], dex_type=row[2])
+    if DrinkingExperiment.objects.filter(dex_date=row[0], cohort=monkey.cohort, dex_type=row[2]).count() == 0:
+      drinking_experiment = DrinkingExperiment(dex_date=row[0], cohort=monkey.cohort, dex_type=row[2])
       drinking_experiment.save()
       # saving the drinking_experiment will also create the MonkeyToExperiment objects
     else:
       # otherwise get the existing experiment
-      experiment = DrinkingExperiment.objects.get(exp_date=row[0], cohort=row[1], dex_type=row[2])
+      experiment = DrinkingExperiment.objects.get(dex_date=row[0], cohort=monkey.cohort, dex_type=row[2])
     
     # get the MonkeyToDrinkingExperiment object for this row
     mtd = MonkeyToDrinkingExperiment.objects.get(monkey=Monkey.objects.get(mky_real_id=row[3]), drinking_experiment=drinking_experiment)
     
     # add the data to the MonkeyToExperiment object
-    mtd.mtd_etoh_intake = row[4]
+    if row[4]:
+      mtd.mtd_etoh_intake = row[4]
+    else:
+      mtd.mtd_etoh_intake = 0
     mtd.mtd_veh_intake = row[5]
     mtd.mtd_total_pellets = row[7]
     mtd.mtd_weight = row[8]
