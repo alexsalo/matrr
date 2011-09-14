72eaa42491fa0b174b3e0dcf7d03e2dcd29589dc
farro <farro@8ba6b188-5121-491e-b013-2137287ea23c>
2011-09-14 16:42:06 +0000

    Should include all Slavka's changes.  No, really, all of them.
    git-svn-id: svn+ssh://gleek.ecs.baylor.edu/matrr/trunk@282 8ba6b188-5121-491e-b013-2137287ea23c

--- matrr/forms.py
+++ matrr/forms.py
@@ -25,7 +25,7 @@ class TissueRequestForm(ModelForm):
 		self.req_request = req_request
 		self.tissue = tissue
 		super(TissueRequestForm, self).__init__(*args, **kwargs)
-		self.fields['monkeys'].widget = CheckboxSelectMultipleLink(link_base='/monkeys/', tissue=self.tissue)
+		self.fields['monkeys'].widget = CheckboxSelectMultipleLinkByTable(link_base='/monkeys/', tissue=self.tissue)
 		self.fields['monkeys'].queryset = self.req_request.cohort.monkey_set.all()
 		# change the help text to match the checkboxes
 		self.fields['monkeys'].help_text =\
@@ -139,7 +139,7 @@ class ContactUsForm(Form):
 class TissueRequestProcessForm(ModelForm):
 	def __init__(self, *args, **kwargs):
 		super(ModelForm, self).__init__(*args, **kwargs)
-		self.fields['accepted_monkeys'].widget = CheckboxSelectMultipleLink(link_base='/monkeys/',
+		self.fields['accepted_monkeys'].widget = CheckboxSelectMultipleLinkByTable(link_base='/monkeys/',
 																			tissue=self.instance.get_tissue())
 		self.fields['accepted_monkeys'].required = False
 		self.fields['accepted_monkeys'].queryset = self.instance.monkeys.all()
--- matrr/templatetags/review_tags.py
+++ matrr/templatetags/review_tags.py
@@ -4,14 +4,9 @@ register = template.Library()
 from settings import STATIC_URL
 
 @register.inclusion_tag('review/collision_requests.html')
-def pokus(request, monkey):
-#    import pdb
-#    pdb.set_trace()
+def colliding_requests(request, monkey):
     collisions = request.get_sub_req_collisions_for_monkey(monkey)
-#    collisions = Request.objects.filter(request_status=4)
-#    for collision in collisions:
-#        print collision.review_set.all()
-#    print request.review_set.all()
+
 
     return {
             'collisions' : collisions,
