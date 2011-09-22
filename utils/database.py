b1a4910c94b0c91c11192729a6c7e03e9c992589
farro <Jonathan_Farro@baylor.edu>
2011-09-22 14:41:06 -0500

    Assay Cohort --> Assay Development

--- utils/database.py
+++ utils/database.py
@@ -760,7 +760,7 @@ def load_mtd(file_name, dex_type='Coh8_initial', cohort_name='INIA Cyno 8'):
 
 def create_Assay_Development_tree():
 	institution = Institution.objects.all()[0]
-	cohort = Cohort.objects.get_or_create(coh_cohort_name="Assay Cohort", coh_upcoming=False, institution=institution)
+	cohort = Cohort.objects.get_or_create(coh_cohort_name="Assay Development", coh_upcoming=False, institution=institution)
 	monkey = Monkey.objects.get_or_create(mky_real_id=0, mky_drinking=False, cohort=cohort[0])
 	for tt in TissueType.objects.exclude(category__cat_name__icontains="Internal"):
 		tissue_sample = TissueSample.objects.get_or_create(tissue_type=tt, monkey=monkey[0])
