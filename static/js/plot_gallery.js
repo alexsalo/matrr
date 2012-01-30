6dd7870a0c5456807682d15e7f975de2347a16b3
farro <Jonathan_Farro@baylor.edu>
2012-01-30 10:56:27 -0600

    Unsupported browser blackout

--- static/js/plot_gallery.js
+++ static/js/plot_gallery.js
@@ -1,7 +1,7 @@
 		$(document).ready(function(){
 			//Examples of how to assign the ColorBox event to elements
-			$("a[rel='plot_gallery']").colorbox({opacity:0.5, maxHeight:"100%", maxWidth:"100%", fixed:true, initialWidth:"100%"});
-				
+			$("a[rel='plot_gallery']").colorbox({opacity:0.5, maxHeight:"100%", maxWidth:"100%", fixed:true, initialWidth:"100%", initialHeight:"100%"});
+
 			//Example of preserving a JavaScript event for inline calls.
 			$("#click").click(function(){ 
 				$('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
