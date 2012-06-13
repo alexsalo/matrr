ca657443b9ff32bae3150a65432993ab80c8ed99
farro <Jonathan_Farro@baylor.edu>
2012-06-13 15:03:05 -0500

    Send 1 shipment ready email

--- utils/regular_tasks/send_shipment_ready.py
+++ utils/regular_tasks/send_shipment_ready.py
@@ -11,11 +11,19 @@ from matrr.models import RequestStatus, Request
 from matrr.emails import send_shipment_ready_notification
 
 def shipments_ready():
+	assay_ready = False
+	send_email = False
 	accepted = Q(req_status__in=[RequestStatus.Partially, RequestStatus.Accepted]) # Requests that have been accepted
 	accepted = Request.objects.filter(accepted)
 	for req in accepted:
 		if req.can_be_shipped():
-			send_shipment_ready_notification(req)
+			send_email = True
+			if 'assay' in req.cohort.coh_cohort_name.lower():
+				assay_ready = True
 
-if settings.PRODUCTION:
-	shipments_ready()
+	return send_email, assay_ready
+
+send_email, assay_ready = shipments_ready()
+if settings.PRODUCTION and send_email:
+	send_shipment_ready_notification(assay_ready=assay_ready)
+
