a2733b5ecbbce9c3a8bc182de4e6b1121eeac27a
farro <Jonathan_Farro@baylor.edu>
2012-12-19 12:14:09 -0600

    modified the dupilicateable cohorts list

--- matrr/views.py
+++ matrr/views.py
@@ -1031,7 +1031,7 @@ def order_duplicate(request, req_request_id):
 			if rtt.accepted_monkeys.all().count() == req.cohort.monkey_set.all().count():
 				tissues.append(rtt.tissue_type)
 
-	queryset = Cohort.objects.exclude(pk=req.pk).order_by('coh_cohort_name')
+	queryset = Cohort.objects.exclude(pk=req.cohort.pk).order_by('coh_cohort_name')
 	return render_to_response('matrr/order/order_duplicate.html', {'req_id': req_request_id, 'cohort_form': CohortSelectForm(subject_queryset=queryset, subject_widget=widgets.Select)}, context_instance=RequestContext(request))
 
 
