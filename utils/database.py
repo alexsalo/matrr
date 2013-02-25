7c5eaa05658fdf504c162380b0280087289a4b70
farro <Jonathan_Farro@baylor.edu>
2013-02-25 12:41:20 -0600

    Don't check to see if eev exists.

--- utils/database.py
+++ utils/database.py
@@ -1,3 +1,4 @@
+from django.db.transaction import commit_on_success
 from matrr.models import *
 from django.db import transaction
 from datetime import datetime as dt
@@ -1284,11 +1285,11 @@ def load_eev_one_file(file_name, dex_type, date):
 				msg = ERROR_OUTPUT % (line_number, "Filename date does not match line date.  Will use line date. filename_date=%s" % str(date), line)
 				logging.info(msg)
 
-			eevs = ExperimentEvent.objects.filter(monkey=monkey, dex_type=dex_type, eev_source_row_number=line_number, eev_occurred=eev_date)
-			if eevs.count() != 0:
-				msg = ERROR_OUTPUT % (line_number, "EEV with monkey, dex_type, date occurred and source row number already exists.", line)
-				logging.debug(msg)
-				continue
+#			eevs = ExperimentEvent.objects.filter(monkey=monkey, dex_type=dex_type, eev_source_row_number=line_number, eev_occurred=eev_date)
+#			if eevs.count() != 0:
+#				msg = ERROR_OUTPUT % (line_number, "EEV with monkey, dex_type, date occurred and source row number already exists.", line)
+#				logging.debug(msg)
+#				continue
 
 			eev = ExperimentEvent()
 			eev.monkey = monkey
@@ -1323,6 +1324,7 @@ def load_eev_one_file(file_name, dex_type, date):
 				continue
 			eev.save()	
 
+@commit_on_success
 def load_eevs(file_dir, dex_type):
 	if not dex_type in DexTypes:
 		raise Exception("'%s' is not an acceptable drinking experiment type.  Please choose from:  %s" % (dex_type, DexTypes))
