f442be5afb56d0bba03e390825bfe3d36e1d90c8
farro <Jonathan_Farro@baylor.edu>
2013-03-07 09:01:19 -0600

    just noticed the date format for uploaded files was wrong.  fixed.

--- matrr/views.py
+++ matrr/views.py
@@ -2726,7 +2726,7 @@ def raw_data_upload(request):
 		form = RawDataUploadForm(request.POST, request.FILES)
 		if form.is_valid():
 			f = request.FILES['data']
-			name = datetime.now().strftime("%m-%d-%Y-%H-%M-%S") + "." + f.name
+			name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + "." + f.name
 			upload_path = os.path.join(settings.UPLOAD_DIR, name)
 			destination = open(upload_path, 'wb+')
 			for chunk in f.chunks():
