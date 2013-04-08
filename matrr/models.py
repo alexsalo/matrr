66678c6ba034b34cd97444669b9d0ae2398da017
farro <Jonathan_Farro@baylor.edu>
2013-04-08 15:10:15 -0500

    When populating the time since last pellet for a bout, handle None values better

--- matrr/models.py
+++ matrr/models.py
@@ -753,7 +753,8 @@ class ExperimentBout(models.Model):
 			previous_events = ExperimentEvent.objects.filter(monkey=self.mtd.monkey, eev_session_time__lt=self.ebt_start_time).order_by('eev_session_time')
 			previous_pellets = previous_events.filter(eev_event_type=ExperimentEventType.Pellet)
 			pellet_max = previous_pellets.aggregate(Max('eev_session_time'))['eev_session_time__max']
-			self.eev_pellet_elapsed_time_since_last = self.ebt_start_time - pellet_max
+			pellet_time = self.ebt_start_time - pellet_max if pellet_max else 0
+			self.eev_pellet_elapsed_time_since_last = pellet_time
 			self.save()
 
 	def populate_contains_pellet(self, recalculate=False):
