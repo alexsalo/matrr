3dfe895163f07b2fdf3a9989c7ff40d84c5cd703
farro <Jonathan_Farro@baylor.edu>
2013-04-08 15:16:59 -0500

    damn typos

--- matrr/models.py
+++ matrr/models.py
@@ -754,7 +754,7 @@ class ExperimentBout(models.Model):
 			previous_pellets = previous_events.filter(eev_event_type=ExperimentEventType.Pellet)
 			pellet_max = previous_pellets.aggregate(Max('eev_session_time'))['eev_session_time__max']
 			pellet_time = self.ebt_start_time - pellet_max if pellet_max else 0
-			self.eev_pellet_elapsed_time_since_last = pellet_time
+			self.ebt_pellet_elapsed_time_since_last = pellet_time
 			self.save()
 
 	def populate_contains_pellet(self, recalculate=False):
