dbade7a34b1500d8b3578d7672596b4e65606937
farro <Jonathan_Farro@baylor.edu>
2013-05-28 14:38:42 -0500

    removed deprecated fields 'req_request.req_report_asked' and 'req_request.req_request_report_asked_count'

--- matrr/models.py
+++ matrr/models.py
@@ -1554,14 +1554,6 @@ class Request(models.Model, DiffingMixin):
 		blank=False, null=False)
 	req_referred_by = models.CharField('How did you hear about the tissue bank?', choices=REFERRAL_CHOICES, null=False, max_length=100)
 	req_notes = models.TextField('Request Notes', null=True, blank=True)
-	req_report_asked = models.BooleanField('Progress report asked', default=False)
-	req_report_asked_count = models.IntegerField('Progress report requested count', default=0)
-
-	def _migrate_report_count(self):
-		if not self.req_report_asked_count and self.req_report_asked:
-			self.req_report_asked_count = 1
-			self.save()
-
 	req_purchase_order = models.CharField("Purchase Order", max_length=200, null=True, blank=True)
 
 	def __unicode__(self):
