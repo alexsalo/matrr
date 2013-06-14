72a28d3b6cf0437eff12bb064dbaf63c8ffd706f
farro <Jonathan_Farro@baylor.edu>
2013-06-14 12:08:20 -0500

    test of permission denied image

--- matrr/views.py
+++ matrr/views.py
@@ -2766,7 +2766,7 @@ def sendfile(request, id):
 				file = getattr(r[0], f)
 			break
 	if not file:
-		raise HttpResponseRedirect('/static/images/MATRR_logo.png')
+		return HttpResponseRedirect('/static/images/MATRR_logo.png')
 
 	if file.url.count('/media') > 0:
 		file_url = file.url.replace('/media/', '')
