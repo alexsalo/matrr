941ef4fe7cb3d5a59f3081c3732553ffaf033298
farro <Jonathan_Farro@baylor.edu>
2013-06-24 15:21:46 -0500

    rewrote RhesusMonkeyBeyesDataset, RhesusBeyesDataset, and  RhesusBeyesDataset_Stage3.
     _Stage3 now properly inherits from RhesusBeyesDataset.  RhesusBeyesDataset should only query the database when initialized.  After that all values should be stored in, and sampled from, memory

--- utils/prediction/datasets.py
+++ utils/prediction/datasets.py
@@ -1,6 +1,55 @@
 __author__ = 'developer'
 import numpy
 
+mtd_columns = [
+	'mtd_etoh_intake',
+	'mtd_veh_intake',
+	'mtd_total_pellets',
+	'mtd_weight',
+	'mtd_pct_etoh',
+	'mtd_etoh_g_kg',
+	'mtd_etoh_bout',
+	'mtd_etoh_drink_bout',
+	'mtd_etoh_mean_drink_length',
+	'mtd_etoh_median_idi',
+	'mtd_etoh_mean_drink_vol',
+	'mtd_etoh_mean_bout_length',
+	'mtd_etoh_media_ibi',
+	'mtd_etoh_mean_bout_vol',
+	'mtd_etoh_st_1',
+	'mtd_etoh_st_2',
+	'mtd_etoh_st_3',
+	'mtd_veh_st_2',
+	'mtd_veh_st_3',
+	'mtd_pellets_st_1',
+	'mtd_pellets_st_3',
+	'mtd_length_st_1',
+	'mtd_length_st_2',
+	'mtd_length_st_3',
+	'mtd_vol_1st_bout',
+	'mtd_pct_etoh_in_1st_bout',
+	'mtd_drinks_1st_bout',
+	'mtd_mean_drink_vol_1st_bout',
+	'mtd_fi_wo_drinking_st_1',
+	'mtd_pct_fi_with_drinking_st_1',
+	'mtd_latency_1st_drink',
+	'mtd_st_1_ioc_avg',
+	'mtd_max_bout',
+	'mtd_max_bout_start',
+	'mtd_max_bout_end',
+	'mtd_max_bout_length',
+	'mtd_max_bout_vol',
+	'mtd_pct_max_bout_vol_total_etoh',
+	'mtd_seconds_to_stageone',
+	'mtd_mean_seconds_between_meals',
+	'mhm_record__mhm_cort',
+	'mhm_record__mhm_acth',
+	'mhm_record__mhm_t',
+	'mhm_record__mhm_doc',
+	'mhm_record__mhm_ald',
+	'mhm_record__mhm_dheas',
+]
+
 class RhesusPCADataset():
 	data = numpy.array([])
 	target = numpy.array([])
@@ -129,59 +178,13 @@ class RhesusPCADataset():
 def create_dataset_from_mtds(mtds):
 	from matrr.models import MonkeyHormone, Avg
 	from utils import plotting_beta
-	data_columns = \
-		['mtd_etoh_intake',
-		 'mtd_veh_intake',
-		 'mtd_total_pellets',
-		 'mtd_weight',
-		 'mtd_pct_etoh',
-		 'mtd_etoh_g_kg',
-		 'mtd_etoh_bout',
-		 'mtd_etoh_drink_bout',
-		 'mtd_etoh_mean_drink_length',
-		 'mtd_etoh_median_idi',
-		 'mtd_etoh_mean_drink_vol',
-		 'mtd_etoh_mean_bout_length',
-		 'mtd_etoh_media_ibi',
-		 'mtd_etoh_mean_bout_vol',
-		 'mtd_etoh_st_1',
-		 'mtd_etoh_st_2',
-		 'mtd_etoh_st_3',
-		 'mtd_veh_st_2',
-		 'mtd_veh_st_3',
-		 'mtd_pellets_st_1',
-		 'mtd_pellets_st_3',
-		 'mtd_length_st_1',
-		 'mtd_length_st_2',
-		 'mtd_length_st_3',
-		 'mtd_vol_1st_bout',
-		 'mtd_pct_etoh_in_1st_bout',
-		 'mtd_drinks_1st_bout',
-		 'mtd_mean_drink_vol_1st_bout',
-		 'mtd_fi_wo_drinking_st_1',
-		 'mtd_pct_fi_with_drinking_st_1',
-		 'mtd_latency_1st_drink',
-		 'mtd_st_1_ioc_avg',
-		 'mtd_max_bout',
-		 'mtd_max_bout_start',
-		 'mtd_max_bout_end',
-		 'mtd_max_bout_length',
-		 'mtd_max_bout_vol',
-		 'mtd_pct_max_bout_vol_total_etoh',
-		 'mtd_seconds_to_stageone',
-		 'mtd_mean_seconds_between_meals',
-		 'mhm_cort',
-		 'mhm_acth',
-		 'mhm_t',
-		 'mhm_doc',
-		 'mhm_ald',
-		 'mhm_dheas'
-		 ]
-	loop_mtd_columns = data_columns[:40]
-	loop_mhm_columns = data_columns[40:]
+	loop_mtd_columns = mtd_columns
+	loop_mhm_columns = mhm_columns
 
 	means = dict()
 	for col in loop_mtd_columns:
+		if col == 'mhm_record':
+			continue
 		mean = mtds.aggregate(Avg(col))[col+'__avg']
 		if not mean:
 			means[col] = mean
@@ -217,136 +220,102 @@ def create_dataset_from_mtds(mtds):
 		data.append(row)
 	return data, target
 
+def create_dataset_from_mtd_values(monkey_pk, mtd_values):
+	from utils import plotting_beta
+	means = dict()
+	for col in mtd_columns:
+		_sum = 0
+		_count = 0
+		for row in mtd_values:
+			if row[col]:
+				_count += 1
+				_sum += row[col]
+		_avg = _sum / float(_count)
+		means[col] = _avg
+
+	target = list()
+	data = list()
+	for mtd_row in mtd_values:
+		target.append(plotting_beta.rhesus_monkey_category[monkey_pk])
+
+		data_row = list()
+		for col in mtd_columns:
+			value = mtd_row[col]
+			if not value and not 'mhm_record' in col:
+				value = means[col] # replace empty MTD records with the average, but do NOT replace empty MHM records
+			if value is None:
+				value = 0
+			data_row.append(value)
+		data.append(data_row)
+	return data, target
+
 
 class RhesusMonkeyBeyesDataset():
 	monkey_pk = 0
-	monkey_mtds = None
+	monkey_values = None
 
-	def __init__(self, mtds):
-		self.monkey_mtds = mtds
-		self.monkey_pk = mtds[0].monkey.pk
+	def __init__(self, monkey_pk, mtd_values):
+		self.monkey_values = mtd_values
+		self.monkey_pk = monkey_pk
 
 	def get_aggregate_dataset(self):
-		from matrr.models import MonkeyHormone, Avg
-		data_columns = \
-			['mtd_etoh_intake',
-			 'mtd_veh_intake',
-			 'mtd_total_pellets',
-			 'mtd_weight',
-			 'mtd_pct_etoh',
-			 'mtd_etoh_g_kg',
-			 'mtd_etoh_bout',
-			 'mtd_etoh_drink_bout',
-			 'mtd_etoh_mean_drink_length',
-			 'mtd_etoh_median_idi',
-			 'mtd_etoh_mean_drink_vol',
-			 'mtd_etoh_mean_bout_length',
-			 'mtd_etoh_media_ibi',
-			 'mtd_etoh_mean_bout_vol',
-			 'mtd_etoh_st_1',
-			 'mtd_etoh_st_2',
-			 'mtd_etoh_st_3',
-			 'mtd_veh_st_2',
-			 'mtd_veh_st_3',
-			 'mtd_pellets_st_1',
-			 'mtd_pellets_st_3',
-			 'mtd_length_st_1',
-			 'mtd_length_st_2',
-			 'mtd_length_st_3',
-			 'mtd_vol_1st_bout',
-			 'mtd_pct_etoh_in_1st_bout',
-			 'mtd_drinks_1st_bout',
-			 'mtd_mean_drink_vol_1st_bout',
-			 'mtd_fi_wo_drinking_st_1',
-			 'mtd_pct_fi_with_drinking_st_1',
-			 'mtd_latency_1st_drink',
-			 'mtd_st_1_ioc_avg',
-			 'mtd_max_bout',
-			 'mtd_max_bout_start',
-			 'mtd_max_bout_end',
-			 'mtd_max_bout_length',
-			 'mtd_max_bout_vol',
-			 'mtd_pct_max_bout_vol_total_etoh',
-			 'mtd_seconds_to_stageone',
-			 'mtd_mean_seconds_between_meals',
-			 'mhm_cort',
-			 'mhm_acth',
-			 'mhm_t',
-			 'mhm_doc',
-			 'mhm_ald',
-			 'mhm_dheas'
-			 ]
-		loop_mtd_columns = data_columns[:40]
-		loop_mhm_columns = data_columns[40:]
-
-		means = list()
-		for col in loop_mtd_columns:
-			mean = self.monkey_mtds.aggregate(Avg(col))[col+'__avg']
-			means.append(mean)
-
-		mtd_dates = self.monkey_mtds.dates('drinking_experiment__dex_date', 'day').distinct()
-		mtd_dates = list(mtd_dates)
-		mhms = MonkeyHormone.objects.filter(monkey=self.monkey_pk, mhm_date__in=mtd_dates)
-		for col in loop_mhm_columns:
-			mean = mhms.aggregate(Avg(col))[col+'__avg']
-			means.append(mean)
+		means = dict()
+		for col in mtd_columns:
+			if col == 'mhm_record':
+				continue
+			_sum = 0
+			for row in self.monkey_values:
+				_sum += row[col] if row[col] else 0
+			avg = _sum / float(len(self.monkey_values))
+			means[col] = avg
 		return means
 
 
 class RhesusBeyesDataset():
+	__mtds = None
+	_monkey_mtd_values= dict()
 	monkey_ids = None
-	monkey_datasets = list()
+	testing_datasets = list()
 	training_dataset = None
 	training_targetset = None
 
-	def __init__(self, train_split=.8, monkey_ids=None):
+	def __init__(self, train_split=.8, monkey_ids=None, mtds=None):
+		from matrr.models import MonkeyToDrinkingExperiment
 		assert 0 < train_split < 1
 		self.train_split = .8
 		if monkey_ids:
 			self.monkey_ids = monkey_ids
+		self.__mtds = mtds if mtds else MonkeyToDrinkingExperiment.objects.Ind().filter(monkey__in=self.monkey_ids).order_by('?')
 		self.create_datasets()
 
 	def create_datasets(self):
-		from matrr.models import MonkeyToDrinkingExperiment
+		for monkey_pk in self.monkey_ids:
+			self._monkey_mtd_values[monkey_pk] = self.__mtds.values(*mtd_columns)
 
+	def sample_dataset(self):
+		import random
+		self.training_dataset = self.training_targetset = None # clear any current datasets
 		training_dataset = list()
 		training_targetset = list()
 		for monkey_pk in self.monkey_ids:
-			induction_mtds = MonkeyToDrinkingExperiment.objects.Ind().filter(monkey=monkey_pk).order_by('?')
-			data_divider = int(induction_mtds.count()*self.train_split)
-			training_mtds = induction_mtds[:data_divider]
-			testing_mtds = induction_mtds[data_divider:]
-			self.monkey_datasets.append(RhesusMonkeyBeyesDataset(testing_mtds))
-			data, target = create_dataset_from_mtds(training_mtds)
+			mky_data = self._monkey_mtd_values[monkey_pk]
+			data_length = len(mky_data)
+			training_length = int(self.train_split * data_length)
+
+			training_indices = sorted(random.sample(xrange(data_length), training_length)) # get random indices
+			testing_indicies = sorted([i for i in xrange(data_length) if i not in training_indices]) # collect the indices that weren't sampled
+			training_sample = [ mky_data[i] for i in training_indices] # collect training dataset from the indices
+			testing_sample = [mky_data[i] for i in testing_indicies] # collect testing dataset from the indices
+			self.testing_datasets.append(RhesusMonkeyBeyesDataset(monkey_pk, testing_sample))
+			data, target = create_dataset_from_mtd_values(monkey_pk, training_sample)
 			training_dataset.extend(data)
 			training_targetset.extend(target)
 		self.training_dataset = numpy.array(training_dataset)
 		self.training_targetset = numpy.array(training_targetset)
 
 
-class RhesusBeyesDataset_Stage3():
-	monkey_ids = None
-	monkey_datasets = list()
-	training_dataset = None
-	training_targetset = None
-
-	def __init__(self, monkey_ids=None):
-		if monkey_ids:
-			self.monkey_ids = monkey_ids
-		self.create_datasets()
-
-	def create_datasets(self):
+class RhesusBeyesDataset_Stage3(RhesusBeyesDataset):
+	def __init__(self, *args, **kwargs):
 		from matrr.models import MonkeyToDrinkingExperiment
-
-		training_dataset = list()
-		training_targetset = list()
-		for monkey_pk in self.monkey_ids:
-			stage_3_induction_mtds = MonkeyToDrinkingExperiment.objects.Ind().filter(monkey=monkey_pk, mtd_etoh_g_kg__gt=1.3)
-			self.monkey_datasets.append(RhesusMonkeyBeyesDataset(stage_3_induction_mtds))
-			data, target = create_dataset_from_mtds(stage_3_induction_mtds)
-			training_dataset.extend(data)
-			training_targetset.extend(target)
-		self.training_dataset = numpy.array(training_dataset)
-		self.training_targetset = numpy.array(training_targetset)
-
-
+		mtds = MonkeyToDrinkingExperiment.objects.Ind().filter(mtd_etoh_g_kg__gt=1.3)
+		super(RhesusBeyesDataset_Stage3, self).__init__(mtds=mtds, *args, **kwargs)
