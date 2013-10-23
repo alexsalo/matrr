12b0c71a31161e00d35df7ea9fd83502a60eb4f6
farro <jonathan_farro@baylor.edu>
2013-10-23 11:42:40 -0500

    added some notes to a deprecation situation, with a suggested solution (so I don't lose it)

--- utils/regular_tasks/send_colliding_requests_info.py
+++ utils/regular_tasks/send_colliding_requests_info.py
@@ -1,9 +1,12 @@
 import sys, os
 project =  os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append(project)
-from django.core.management import setup_environ
 from matrr import settings
+
+# deprecated
+from django.core.management import setup_environ
 setup_environ(settings)
+# test this solution:  http://stackoverflow.com/questions/17937017/updated-django-from-1-4-to-1-5-1-now-deprecationwarning-on-setup-environ
 
 from matrr.emails import send_colliding_requests_info
 
--- utils/regular_tasks/urge_po_mta.py
+++ utils/regular_tasks/urge_po_mta.py
@@ -3,7 +3,11 @@ project =  os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__fi
 sys.path.append(project)
 from django.core.management import setup_environ
 from matrr import settings
+
+# deprecated
+from django.core.management import setup_environ
 setup_environ(settings)
+# test this solution:  http://stackoverflow.com/questions/17937017/updated-django-from-1-4-to-1-5-1-now-deprecationwarning-on-setup-environ
 
 from matrr.emails import urge_po_mta
 
