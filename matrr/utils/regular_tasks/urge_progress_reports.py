1f9986cdc45e9d6430a21b69601dd10d0f8537af
matrr-prod <matrrgleek@gmail.com>
2013-11-04 08:09:46 -0600

    commented out DEBUG=True in settings, and fixed a matrr-local path bug

--- matrr/utils/regular_tasks/urge_progress_reports.py
+++ matrr/utils/regular_tasks/urge_progress_reports.py
@@ -1,10 +1,13 @@
 import sys
 import os
-sys.path.append('/web/www/matrr-local')
+print sys.path
+sys.path.append('/web/www/matrr-prod')
+print sys.path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "matrr.settings")
 
 from matrr import settings
 from matrr.emails import urge_progress_reports
 
 if settings.PRODUCTION:
+	raise Exception()
 	urge_progress_reports()
