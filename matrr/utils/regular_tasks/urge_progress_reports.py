c495839c138537f65aa9a14a987b1feaca6f42dc
matrr-prod <matrrgleek@gmail.com>
2013-11-04 08:40:55 -0600

    removed debug prints

--- matrr/utils/regular_tasks/urge_progress_reports.py
+++ matrr/utils/regular_tasks/urge_progress_reports.py
@@ -1,8 +1,6 @@
 import sys
 import os
-print sys.path
 sys.path.append('/web/www/matrr-prod')
-print sys.path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "matrr.settings")
 
 from matrr import settings
