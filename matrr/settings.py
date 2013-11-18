6d3acedd8374c3b1cf5449ac3716c0bb4bd95d15
farro <jonathan_farro@baylor.edu>
2013-11-18 13:56:18 -0600

    maybe added logging?

--- matrr/settings.py
+++ matrr/settings.py
@@ -242,6 +242,12 @@ ResearchUpdateInProgressGrace = 180
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+         'require_debug_false': {
+             '()': 'django.utils.log.RequireDebugFalse'
+         }
+    },
+
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
