bf2fabf75ee3a8d5d3799c2ee268158289b233c9
farro <jonathan_farro@baylor.edu>
2014-01-22 09:00:34 -0600

    These changes were made yesterday.  Working on the PCA stuff, I factored out a lot of kwargs into a config object and created a new scatterplot with kmeans2 centroids...

--- matrr/utils/pca.py
+++ matrr/utils/pca.py
@@ -4,6 +4,7 @@ from matrr import models, plotting
 
 
 def swoon(field_list, cohort_pk=8, colors=False, MATRR_MODEL=models.MonkeyToDrinkingExperiment):
+    categories = list()
     color_values = list()
 
     data = MATRR_MODEL.objects.OA().exclude_exceptions().filter(monkey__cohort=cohort_pk)
@@ -15,7 +16,7 @@ def swoon(field_list, cohort_pk=8, colors=False, MATRR_MODEL=models.MonkeyToDrin
 #        if None in row:
 #            data.pop(index)
     data = numpy.array(data, dtype=object)
-    return data, color_values
+    return data, categories, color_values
 
 def matplotlib_pca_test():
     from matplotlib import mlab
@@ -59,7 +60,7 @@ def mpl_3d_pca():
     #construct your numpy array of data
 #    fields = ['mtd_etoh_intake', 'mtd_veh_intake', 'mtd_total_pellets', 'mtd_etoh_media_ibi', 'mtd_pct_etoh_in_1st_bout']
     fields = ['mtd_etoh_intake', 'mtd_veh_intake', 'mtd_total_pellets', 'mtd_pct_etoh', 'mtd_etoh_g_kg', 'mtd_etoh_bout', 'mtd_etoh_drink_bout', 'mtd_etoh_mean_drink_length', 'mtd_etoh_median_idi', 'mtd_etoh_mean_drink_vol', 'mtd_etoh_mean_bout_length', 'mtd_etoh_media_ibi', 'mtd_etoh_mean_bout_vol', 'mtd_vol_1st_bout', 'mtd_pct_etoh_in_1st_bout', 'mtd_drinks_1st_bout', 'mtd_mean_drink_vol_1st_bout', 'mtd_latency_1st_drink', 'mtd_max_bout', 'mtd_max_bout_start', 'mtd_max_bout_end', 'mtd_max_bout_length', 'mtd_max_bout_vol', 'mtd_pct_max_bout_vol_total_etoh']
-    myData, colors = swoon(fields, colors=True)
+    myData, categories, colors = swoon(fields, colors=True)
     results = PCA(myData)
 
     x = []
@@ -101,18 +102,38 @@ def find_empty_columns(cohort=8, empty_threshold=.5, MATRR_MODEL=models.MonkeyTo
             full_columns.append(field)
     return full_columns, empty_columns
 
-def matrr_pca(cohort=8, empty_column_threshold=.5, MATRR_MODEL=models.MonkeyBEC):
-    from matplotlib import mlab
-    full_columns, empty_columns = find_empty_columns(cohort, empty_column_threshold, MATRR_MODEL)
-    print full_columns
+class ConfigObjectNeedsBetterName(object):
+    cohort = 8
+    matrr_model = models.MonkeyBEC
+    empty_column_threshold = .5
 
-    data, colors = swoon(full_columns, cohort_pk=cohort, MATRR_MODEL=MATRR_MODEL, colors=True)
-    data = numpy.array(data, dtype=float)
+    def __init__(self, cohort=8, matrr_model=models.MonkeyBEC, empty_column_threshold=.5):
+        self.cohort = cohort
+        self.matrr_model = matrr_model
+        self.empty_column_threshold = empty_column_threshold
+
+def PCA_data_collection_all_nonempty_model_fields(cfg=ConfigObjectNeedsBetterName()):
+    from matplotlib import mlab
+    full_columns, empty_columns = find_empty_columns(cfg.cohort, cfg.empty_column_threshold, cfg.matrr_model)
+
+    data, categories, colors = swoon(full_columns, cohort_pk=cfg.cohort, MATRR_MODEL=cfg.matrr_model, colors=True)
+    data = numpy.ma.masked_invalid(numpy.array(data, dtype=float))
     pca = mlab.PCA(data)
+
+    print 'First Principle Component:  '
+    for i in range(2):
+        print "Principle Component %d" % i
+        for index,column in enumerate(full_columns):
+            print "%s:  %f" % (column, pca.Wt[5][index])
+
     first_principle_component = pca.Wt[0] * data
     second_principle_component = pca.Wt[1] * data
     X_axis = second_principle_component.sum(axis=1)
     Y_axis = first_principle_component.sum(axis=1)
+    return X_axis, Y_axis, categories, colors
+
+def matrr_pca_scatter(cfg=ConfigObjectNeedsBetterName()):
+    X_axis, Y_axis, categories, colors = PCA_data_collection_all_nonempty_model_fields(cfg=cfg)
 
     from matrr.utils import plotting_beta
     fig = pyplot.figure(figsize=plotting_beta.DEFAULT_FIG_SIZE, dpi=plotting_beta.DEFAULT_DPI)
@@ -126,4 +147,30 @@ def matrr_pca(cohort=8, empty_column_threshold=.5, MATRR_MODEL=models.MonkeyBEC)
     main_plot.scatter(X_axis, Y_axis, color=colors, s=30)
     return fig
 
+def matrr_pca_scatter_centroids(cfg=ConfigObjectNeedsBetterName()):
+    import collections
+    from scipy import cluster
+    from matrr.utils import plotting_beta
+
+    fig = pyplot.figure(figsize=plotting_beta.DEFAULT_FIG_SIZE, dpi=plotting_beta.DEFAULT_DPI)
+    main_gs = plotting_beta.gridspec.GridSpec(3, 40)
+    main_gs.update(left=0.08, right=.98, wspace=0, hspace=0)
+    main_plot = fig.add_subplot(main_gs[:, :])
+    main_plot.set_title("ScatterCentroids")
+    main_plot.set_xlabel("PC2 Centroids")
+    main_plot.set_ylabel("PC1 Centroids")
+
+    unorganized_data = PCA_data_collection_all_nonempty_model_fields(cfg=cfg)
+    organized_data = collections.defaultdict(lambda: list())
+    for datapoint in zip(*unorganized_data):
+        X_value, Y_value, category, color = datapoint
+        organized_data[category].append((X_value, Y_value))
+    for category in organized_data.keys():
+        plot_data = numpy.array(organized_data[category])
+        res, idx = cluster.vq.kmeans2(plot_data, 1)
+        centroid_xaxis = res[:, 0]
+        centroid_yaxis = res[:, 1]
+        main_plot.scatter(centroid_xaxis, centroid_yaxis, color=plotting.RHESUS_COLORS[category], s=150)
+        main_plot.scatter(centroid_xaxis, centroid_yaxis, color=plotting.RHESUS_COLORS[category], s=300, marker='x')
+    return fig
 
