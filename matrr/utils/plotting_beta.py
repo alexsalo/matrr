7a4994f6ddcd94db47ce19ee6c378c1af1dc569e
farro <jonathan_farro@baylor.edu>
2014-01-27 14:10:32 -0600

    Man, I dunno if this is gonna be faster, but it's at least different and might should be running.

--- matrr/utils/plotting_beta.py
+++ matrr/utils/plotting_beta.py
@@ -1557,7 +1557,7 @@ def eev_gkg_summation_by_minute_general(monkey_set, minutes=20, minutes_gap=1, D
         diff = (datetime.now() - md_start).seconds
         _duration_minutes = diff / 60
         _duration_seconds = diff % 60
-        print "%s:  Iteration Duration = %dm%ds" % (str(datetime.now()), _monkey, str(_date), _duration_minutes, _duration_seconds)
+        print "%s:  Iteration Duration = %dm%ds" % (str(datetime.now()), _duration_minutes, _duration_seconds)
     print "Damn yo, that shit finally finished.  Next round's on me."
     return gkg_by_minute_from_pellet
 
