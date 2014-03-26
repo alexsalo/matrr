9192af0a1eb2605ea4c7157fca9080126bc20011
farro <jonathan_farro@baylor.edu>
2014-03-26 09:18:17 -0500

    Whitespace changes, converted tabs to spaces in plotting.specific_callables

--- matrr/plotting/specific_callables.py
+++ matrr/plotting/specific_callables.py
@@ -6,78 +6,78 @@ they can be quite useful, especially for reducing/eliminating redundant code.
 
 """
 __author__ = 'developer'
-from matrr.models import NecropsySummary
+from matrr.models import NecropsySummary, MonkeyBEC
 
 
 def etoh_intake(queryset):
-	return queryset.exclude(mtd_etoh_intake=None).values_list('mtd_etoh_intake')
+    return queryset.exclude(mtd_etoh_intake=None).values_list('mtd_etoh_intake')
 
 def total_pellets(queryset):
-	return queryset.exclude(mtd_total_pellets=None).values_list('mtd_total_pellets')
+    return queryset.exclude(mtd_total_pellets=None).values_list('mtd_total_pellets')
 
 def veh_intake(queryset):
-	return queryset.exclude(mtd_veh_intake=None).values_list('mtd_veh_intake')
+    return queryset.exclude(mtd_veh_intake=None).values_list('mtd_veh_intake')
 
 def mtd_weight(queryset):
-	return queryset.exclude(mtd_weight=None).values_list('mtd_weight')
+    return queryset.exclude(mtd_weight=None).values_list('mtd_weight')
 
 def necropsy_summary_avg_22hr_g_per_kg(queryset):
-	"""
-	This method, used by necropsy graphs, will collect 12 month and 6 month average g/kg etoh intake for the [queryset]
+    """
+    This method, used by necropsy graphs, will collect 12 month and 6 month average g/kg etoh intake for the [queryset]
 
-	Args:
-	queryset is expected to be a queryset of monkeys
+    Args:
+    queryset is expected to be a queryset of monkeys
 
-	return:
-	tuple, ( list of 6 month averages, list of 12 month averages, list of monkey pk labels )
-	"""
-	summaries = []
-	raw_labels = []
-	for mky in queryset.order_by("necropsy_summary__ncm_22hr_12mo_avg_g_per_kg", "necropsy_summary__ncm_22hr_6mo_avg_g_per_kg"):
-		try:
-			summaries.append(mky.necropsy_summary)
-		except NecropsySummary.DoesNotExist:
-			continue
-		raw_labels.append(str(mky.pk))
-	return [summary.ncm_22hr_6mo_avg_g_per_kg for summary in summaries], [summary.ncm_22hr_12mo_avg_g_per_kg for summary in summaries], raw_labels
+    return:
+    tuple, ( list of 6 month averages, list of 12 month averages, list of monkey pk labels )
+    """
+    summaries = []
+    raw_labels = []
+    for mky in queryset.order_by("necropsy_summary__ncm_22hr_12mo_avg_g_per_kg", "necropsy_summary__ncm_22hr_6mo_avg_g_per_kg"):
+        try:
+            summaries.append(mky.necropsy_summary)
+        except NecropsySummary.DoesNotExist:
+            continue
+        raw_labels.append(str(mky.pk))
+    return [summary.ncm_22hr_6mo_avg_g_per_kg for summary in summaries], [summary.ncm_22hr_12mo_avg_g_per_kg for summary in summaries], raw_labels
 
 def necropsy_summary_etoh_4pct(queryset):
-	"""
-	This method, used by necropsy graphs, will collect 22hr and lifetime etoh intake (in ml) for the [queryset]
+    """
+    This method, used by necropsy graphs, will collect 22hr and lifetime etoh intake (in ml) for the [queryset]
 
-	Args:
-	queryset is expected to be a queryset of monkeys
+    Args:
+    queryset is expected to be a queryset of monkeys
 
-	return:
-	tuple, ( list of open access total intakes, list of lifetime total intakes, list of monkey pk labels )
-	"""
-	summaries = []
-	raw_labels = []
-	for mky in queryset.order_by("necropsy_summary__ncm_etoh_4pct_lifetime", "necropsy_summary__ncm_etoh_4pct_22hr"):
-		try:
-			summaries.append(mky.necropsy_summary)
-		except NecropsySummary.DoesNotExist:
-			continue
-		raw_labels.append(str(mky.pk))
-	return [summary.ncm_etoh_4pct_22hr for summary in summaries], [summary.ncm_etoh_4pct_lifetime for summary in summaries], raw_labels
+    return:
+    tuple, ( list of open access total intakes, list of lifetime total intakes, list of monkey pk labels )
+    """
+    summaries = []
+    raw_labels = []
+    for mky in queryset.order_by("necropsy_summary__ncm_etoh_4pct_lifetime", "necropsy_summary__ncm_etoh_4pct_22hr"):
+        try:
+            summaries.append(mky.necropsy_summary)
+        except NecropsySummary.DoesNotExist:
+            continue
+        raw_labels.append(str(mky.pk))
+    return [summary.ncm_etoh_4pct_22hr for summary in summaries], [summary.ncm_etoh_4pct_lifetime for summary in summaries], raw_labels
 
 def necropsy_summary_sum_g_per_kg(queryset):
-	"""
-	This method, used by necropsy graphs, will collect 22hr and lifetime etoh intake (in g/kg) for the [queryset]
+    """
+    This method, used by necropsy graphs, will collect 22hr and lifetime etoh intake (in g/kg) for the [queryset]
 
-	Args:
-	queryset is expected to be a queryset of monkeys
+    Args:
+    queryset is expected to be a queryset of monkeys
 
-	return:
-	tuple, ( list of open access total intakes, list of lifetime total intakes, list of monkey pk labels )
-	"""
-	summaries = []
-	raw_labels = []
-	for mky in queryset.order_by("necropsy_summary__ncm_sum_g_per_kg_22hr", "necropsy_summary__ncm_sum_g_per_kg_lifetime"):
-		try:
-			summaries.append(mky.necropsy_summary)
-		except NecropsySummary.DoesNotExist:
-			continue
-		raw_labels.append(str(mky.pk))
-	return [summary.ncm_sum_g_per_kg_22hr for summary in summaries], [summary.ncm_sum_g_per_kg_lifetime for summary in summaries], raw_labels
+    return:
+    tuple, ( list of open access total intakes, list of lifetime total intakes, list of monkey pk labels )
+    """
+    summaries = []
+    raw_labels = []
+    for mky in queryset.order_by("necropsy_summary__ncm_sum_g_per_kg_22hr", "necropsy_summary__ncm_sum_g_per_kg_lifetime"):
+        try:
+            summaries.append(mky.necropsy_summary)
+        except NecropsySummary.DoesNotExist:
+            continue
+        raw_labels.append(str(mky.pk))
+    return [summary.ncm_sum_g_per_kg_22hr for summary in summaries], [summary.ncm_sum_g_per_kg_lifetime for summary in summaries], raw_labels
 
