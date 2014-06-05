aaf4cf756e57084230adc08336617d2d75dbd90b
farro <jonathan_farro@baylor.edu>
2014-06-05 11:00:51 -0500

    fuckit.  I hate 10107, he's a jerk.

--- matrr/utils/parallel_plot.py
+++ matrr/utils/parallel_plot.py
@@ -208,12 +208,12 @@ class MATRRParallelPlot():
 
         x_values = range(1, len(self.parallel_labels)+1)
         for _monkey in self.monkeys:
-            y_values = self.parallel_data[_monkey]
+            y_values = self.parallel_data.get(_monkey, [])
             if len(y_values) != len(x_values):  # we don't have enough data to plot this monkey.  It's probably 10107.
                 continue
             _category = models.Monkey.objects.get(pk=_monkey).mky_drinking_category
             _color = RHESUS_COLORS[_category] if _category else 'black'
-            subplot.plot(x_values, y_values, color=_color, alpha=1)
+            subplot.plot(x_values, y_values, color=_color, alpha=.5)
         subplot.set_title(self.figure_title)
         subplot.set_yticks([.15, .5, .85])
         subplot.set_yticklabels(['Low', 'Med', 'High'])
