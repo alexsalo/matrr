f96e47c546f209024103f421869017f21c6b92c0
farro <jonathan_farro@baylor.edu>
2014-10-22 11:23:43 -0500

    the 1.7 upgrade broke the regular_task scripts.  this should fix it.

--- matrr/utils/regular_tasks/urge_po_mta.py
+++ matrr/utils/regular_tasks/urge_po_mta.py
@@ -2,6 +2,8 @@ import sys
 import os
 sys.path.append('/web/www/matrr-prod')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "matrr.settings")
+import django
+django.setup()
 
 from matrr import settings
 from matrr.emails import urge_po_mta
