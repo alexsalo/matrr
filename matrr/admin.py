99188c297a9823d9d0f856f3039a1036af1dfff5
salo <alexsalovrn@gmail.com>
2016-05-09 14:04:03 -0500

    shipments error 500 fixed - typo found

--- matrr/admin.py
+++ matrr/admin.py
@@ -112,7 +112,7 @@ class ShipmentAdmin(admin.ModelAdmin):
     req_request_cohort.short_description = 'Cohort'
 
     def req_request_user(self, obj):
-        return obj.req_rfequest.user.username
+        return obj.req_request.user.username
     req_request_user.short_description = 'Requesting User'
 
     def req_request_pk(self, obj):
